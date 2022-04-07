308ca7b314a453f8ac547c71b6558bd25250ed18
kishibe-rohan <devrohan2000@gmail.com>
2022-04-07 19:58:57 +0530

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home, AddUser, AddRelation } from "./pages/index";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<AddUser />} />
+        <Route path="/relation" element={<AddRelation />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from "./Home";
+export { default as AddUser } from "./AddUser";
+export { default as AddRelation } from "./AddRelation";
